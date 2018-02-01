88ae1ff3de924f6e18e7cb75462bc0bc083d0a23
Dima Kiman <dimaskiman1993@gmail.com>
2018-02-01 10:58:16 +0300

    test task

---
+++ TestTask/assets/js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC5hZGRFdmVudExpc3RlbmVyKCdET01Db250ZW50TG9hZGVkJywgZnVuY3Rpb24oKXtcclxuXHJcbn0pO1xyXG4iXX0=
